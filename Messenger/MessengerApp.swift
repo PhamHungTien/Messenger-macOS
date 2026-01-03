3e2c0956ce649e52f608724d370a81f785f118fd
Phạm Hùng Tiến <hungtien10a7@gmail.com>
2026-01-03 19:59:37 +0700

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Phạm Hùng Tiến on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
